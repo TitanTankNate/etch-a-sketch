97a9a32c335b2f7b199afa8be91ebc7559d8f528
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-09-02 12:48:25 -0400

    Create 16x16 grid
    + Created iterator that creates all 256 grid blocks
    + N Grid does not automatically grow/shrink as it
    +   should to accommodate different window sizes.

--- javascript.js
+++ javascript.js
@@ -7,18 +7,28 @@
 // Make sure that the grid blocks are square
 // (and maybe proportional to the screen width and height?)
 
+let num1 = (innerHeight*.9)/16-4;
+
 // Select parent container
 const container = document.querySelector(".container");
 
-// Create single grid block
-// // Create div
-const gridBlockTemplate = document.createElement("div");
+for (let i=1;i<=(16 ** 2);i++) {
+    // Create single grid block
+    // // Create div
+    const gridBlockTemplate = document.createElement("div");
 
-// // Make div square
+    // // Make div square
+    gridBlockTemplate.style.display = "flex";
+    gridBlockTemplate.style.flex = `1 1 ${num1}px`;
+    gridBlockTemplate.style.aspectRatio = "1";
 
-// // Style grid block
-gridBlockTemplate.style.backgroundColor = "#00FF00";
-gridBlockTemplate.textContent = "test";
+    // // Style grid block
+    gridBlockTemplate.style.borderStyle = "solid";
+    gridBlockTemplate.style.borderColor = "#00FF00";
+    gridBlockTemplate.style.borderWidth = "2px";
+    gridBlockTemplate.textContent = `t${i}`;
+    // gridBlockTemplate.textContent = '';
 
-// TEST: Append child
-container.appendChild(gridBlockTemplate);
+    // TEST: Append child
+    container.appendChild(gridBlockTemplate);
+}
