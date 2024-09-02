9bd468eaf644350860f4f7c2c1a99b1208d156e6
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-09-02 14:26:13 -0400

    Create better grid
    + Created better grid system using rigid dimensions
    + Added grid creation to function to allow for
    custom grid sizing options

--- javascript.js
+++ javascript.js
@@ -1,34 +1,40 @@
-// Project walkthrough
-// Create 16x16 grid of square divs
 
-// Select parent container
-// Create single grid block
-// Append 16^2 child grid blocks to that container
-// Make sure that the grid blocks are square
-// (and maybe proportional to the screen width and height?)
 
-let num1 = (innerHeight*.9)/16-4;
 
-// Select parent container
-const container = document.querySelector(".container");
+function createGrid(gridSize) {
+    // Select parent container
+    const container = document.querySelector(".container");
 
-for (let i=1;i<=(16 ** 2);i++) {
-    // Create single grid block
-    // // Create div
-    const gridBlockTemplate = document.createElement("div");
+    // Create gridSize x gridSize grid of divs
+    for (let i=1;i<=(gridSize ** 2);i++) {
+        // Create single grid block
+        // // Create div
+        const gridBlockTemplate = document.createElement("div");
+        gridBlockTemplate.classList.add("grid-box");
 
-    // // Make div square
-    gridBlockTemplate.style.display = "flex";
-    gridBlockTemplate.style.flex = `1 1 ${num1}px`;
-    gridBlockTemplate.style.aspectRatio = "1";
+        // // Make div square
+        gridBlockTemplate.style.display = "flex";
+        gridBlockTemplate.style.flex = "1 1 auto";
+        gridBlockTemplate.style.boxSizing = "border-box";
+        gridBlockTemplate.style.aspectRatio = "1";
+        gridBlockTemplate.style.minWidth = `${800/gridSize}px`;
 
-    // // Style grid block
-    gridBlockTemplate.style.borderStyle = "solid";
-    gridBlockTemplate.style.borderColor = "#00FF00";
-    gridBlockTemplate.style.borderWidth = "2px";
-    gridBlockTemplate.textContent = `t${i}`;
-    // gridBlockTemplate.textContent = '';
+        // // Style grid block
+        // gridBlockTemplate.style.borderStyle = "solid";
+        // gridBlockTemplate.style.borderColor = "#00FF00";
+        // gridBlockTemplate.style.borderWidth = "2px";
+        gridBlockTemplate.textContent = `t${i}`;
+        // gridBlockTemplate.textContent = '';
 
-    // TEST: Append child
-    container.appendChild(gridBlockTemplate);
+        // Append child
+        container.appendChild(gridBlockTemplate);
+    }
+
 }
+
+createGrid(16);
+
+const allGridBlocks = document.querySelectorAll(".grid-box");
+allGridBlocks.forEach((gridBlock) => {
+    console.log(gridBlock.textContent);
+});
